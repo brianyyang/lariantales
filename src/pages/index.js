50bb01238a6106c8b7c5c325eeedc4fd984371fb
Brian Yang <b.y.yang212@gmail.com>
2020-11-16 05:36:00 -0500

    force deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ function IndexPage() {
         >
           Prologue
         </a>
-        <Link to="page-2">Post-prologue video</Link>
+        <Link to="page-2">Post-prologue</Link>
       </Prologue>
       <OddChapter>Chapter One</OddChapter>
       <EvenChapter>Chapter Two</EvenChapter>
